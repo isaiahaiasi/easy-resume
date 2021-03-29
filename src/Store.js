51995531665521a1b078725fc7240642eee79d84
isaiahaiasi <isaiah.althouse@gmail.com>
2021-03-28 17:57:11 -0700

    Refactor data store to use hooks
    (note: probably less efficient in this case, as it's all being stored in a single state hook,
    and an entirely new state object is being created every change--rather than the new state property
    simply being merged into the previous state.

--- src/Store.js
+++ src/Store.js
@@ -1,79 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import defaultData from "./demo-data-custom.json";
 import Resume from "./Resume";
 
-const actions = {
+export const actions = {
   add: "ADD",
   get: "GET",
   set: "SET",
-  delete: "DELETE",
+  remove: "REMOVE",
 };
 
 // TODO: add localStorage
-class Store extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = defaultData;
-    this.request = this.request.bind(this);
-  }
+export default function Store() {
+  const [storeData, setStoreData] = useState(defaultData);
 
-  add(id, data) {
-    if (this.state[id]) {
+  const add = (id, data) => {
+    if (storeData[id]) {
       throw new Error(
         `Tried to create a new record at ${id}, but one already exists`
       );
     }
 
-    this.setState({ [id]: data });
-  }
+    setStoreData((prevData) => ({ ...prevData, [id]: data }));
+  };
 
-  get(id) {
-    if (!this.state[id]) {
+  const get = (id) => {
+    if (!storeData[id]) {
       console.warn(`Tried to get data with key ${id}, which does not exist`);
       return null;
     }
 
-    return this.state[id];
-  }
+    return storeData[id];
+  };
 
-  set(id, data) {
-    if (!this.state[id]) {
+  const set = (id, data) => {
+    if (!storeData[id]) {
       throw new Error(
         `Tried to update record at ${id}, but record does not exist`
       );
     }
 
-    this.setState({ [id]: data });
-  }
+    setStoreData((prevData) => ({ ...prevData, [id]: data }));
+  };
 
-  delete(id) {
-    if (!this.state[id]) {
+  const remove = (id) => {
+    if (!storeData[id]) {
       console.warn(`Tried to delete non-existent key ${id}`);
       return null;
     }
 
-    this.setState({ [id]: undefined });
-  }
+    setStoreData((prevData) => ({ ...prevData, [id]: undefined }));
+  };
 
-  request(action, id, data) {
+  const request = (action, id, data) => {
     switch (action) {
       case actions.add:
-        return this.add(id, data);
+        return add(id, data);
       case actions.get:
-        return this.get(id);
+        return get(id);
       case actions.set:
-        return this.set(id, data);
-      case actions.delete:
-        return this.delete(id);
+        return set(id, data);
+      case actions.remove:
+        return remove(id);
       default:
         throw new Error(`Unhandled action type ${action}`);
     }
-  }
+  };
 
-  render() {
-    return <Resume data={this.state} dataHandler={this.request} />;
-  }
+  return <Resume data={storeData} dataHandler={request} />;
 }
-
-export { actions };
-export default Store;
